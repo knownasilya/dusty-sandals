311a8438b977a9175b07d6964f5d810679963dfa
Ilya Radchenko <ilya@burstcreations.com>
2016-01-20 14:56:41 -0500

    Add build step for GH pages

---
+++ deploy-gh-pages.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var exec = require('child_process').exec;
+var RSVP = require('rsvp');
+var execOptions = { cwd: process.cwd() };
+var branch = 'gh-pages';
+
+function checkoutGhPages() {
+  return runCommand('git checkout ' + branch, execOptions);
+}
+
+function copy() {
+  return runCommand('cp -R Dusty\\ Sandals.materials/Release/* .', execOptions);
+}
+
+function addAndCommit() {
+  return runCommand('git add . && git commit -m "build"', execOptions)
+}
+
+function returnToPreviousCheckout() {
+  // Each '\' needed to be escaped here
+  return runCommand("git checkout `git reflog HEAD | sed -n " +
+    "'/checkout/ !d; s/.* \\(\\S*\\)$/\\1/;p' | sed '2 !d'`", execOptions);
+}
+
+checkoutGhPages()
+  .then(copy)
+  .then(addAndCommit)
+  .then(returnToPreviousCheckout)
+  .then(function() {
+    console.log('Done. All that\'s left is to git push the ' + branch +
+      ' branch.\nEx: git push origin ' + branch + '\n');
+  });
+
+function runCommand(/* child_process.exec args */) {
+  var args = Array.prototype.slice.call(arguments);
+
+  var lastIndex = args.length - 1;
+  var lastArg   = args[lastIndex];
+  var logOutput = false;
+  if (typeof lastArg === 'boolean') {
+    logOutput = lastArg;
+    args.splice(lastIndex);
+  }
+
+  return new RSVP.Promise(function(resolve, reject) {
+    var cb = function(err, stdout, stderr) {
+      if (logOutput) {
+        if (stderr) {
+          console.log(stderr);
+        }
+
+        if (stdout) {
+          console.log(stdout);
+        }
+      }
+
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve();
+    };
+
+    args.push(cb);
+    exec.apply(exec, args);
+  }.bind(this));
+}
